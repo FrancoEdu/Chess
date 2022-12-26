a87ace326407ea491e268caa4c737c6ff30395ec
FrancoEdu <franc0edu24@gmail.com>
2022-12-26 15:11:17 -0300

    Second import commit

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 //pricipal classe do sistema
 public class ChessMatch {
@@ -8,16 +11,22 @@ public class ChessMatch {
 
     public ChessMatch(){
         board = new Board(8,8); // definindo tamanho do tabuleiro
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
-        ChessPiece[][] mat = new ChessPiece[board.getRow()][board.getColumn()];
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 
-        for (int i = 0; i < board.getRow(); i++){
-            for (int j = 0; j < board.getColumn();j++){
+        for (int i = 0; i < board.getRows(); i++){
+            for (int j = 0; j < board.getColumns();j++){
                 mat[i][j] = (ChessPiece) board.piece(i,j);
             }
         }
         return mat; // retorna matriz de peças da partida
+    }
+
+    private void initialSetup(){
+        board.placePiece(new Rook(board,Color.WHITE), new Position(4,1));
+        board.placePiece(new King(board,Color.WHITE), new Position(7,3));
     }
 }
